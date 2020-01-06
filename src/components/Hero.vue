f8f1adbddad57ed43d118554a6e3decdf1c2cf1f
Milos Bjelcevic <mbj@valuer.ai>
2020-01-06 23:12:37 +0100

    My first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center hero">
+    <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
+    <h1 class="mb-4">Login</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
